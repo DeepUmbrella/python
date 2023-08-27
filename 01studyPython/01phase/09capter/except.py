83b708567ab38bc96a042619013c7136cd7bb687
linyan <737271441@qq.com>
2023-08-27 15:30:17 +0800

    add exception

--- 01studyPython/01phase/09capter/except.py
+++ 01studyPython/01phase/09capter/except.py
@@ -1,8 +1,43 @@
 
 # catch execption
+f
 
 try:
  f = open("/exception.txt","r",encoding="utf-8")
 except:
  print("found a exception")
  f = open("/exception.txt","w",encoding="utf-8")
+ 
+f.close()
+# catch all exception (显式捕获所有异常)
+try:
+   f = open("/exception.txt","r",encoding="utf-8")     
+except Exception as e:
+   f = open("/exception.txt","w",encoding="utf-8")
+    # (隐式捕获所有异常)
+
+'''
+except:
+ # todo
+'''
+'''
+# catch single exception
+
+try:
+   f = open("/exception.txt","r",encoding="utf-8")     
+except FileNotFoundError as exceptError:
+   f = open("/exception.txt","w",encoding="utf-8") 
+
+
+# try catch else finally
+
+
+try:
+   f = open("/exception.txt","r",encoding="utf-8")    
+except:
+   f = open("/exception.txt","w",encoding="utf-8") 
+else:
+   print("no exception found")
+finally:
+   print("finally i will run this")
+   '''
